254bb019e23d28ca52411d204ac97d9418f52d1d
biomiller <92millero@gmail.com>
2019-08-02 13:23:56 +0100

    cleaned up

--- user_login_app/src/__tests__/App.test.js
+++ user_login_app/src/__tests__/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import { App } from './App.js';
+import { App } from '../App.js';
 
 it('is app rendered', () => {
   const tree=renderer.create(<App />).toTree();
